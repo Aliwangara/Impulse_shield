e0b35f74bc6b0304cf67ed229e0fffe913f0805b
Ali Wangara <Aliwangara63@gmail.com>
2025-07-05 21:51:05 +0300

    changed display functionality dynamically adding delete and goals completed button

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
 const goalBtn = document.getElementById('goal-btn');
 let goalInput = document.getElementById('goal-input');
 let goalList = document.getElementById('goal-list');
+let goalForm = document.getElementById('goal-form');
+const goalInputEl = document.getElementById('goal-input');
+const goalCost = document.getElementById('goal-cost');
+const goalDeadline = document.getElementById('goal-deadline')
+const goalEmoji = document.getElementById('goal-emoji');
+const ImpulseList = document.getElementById('Impulse-list');
+const goalsCompleted = document.getElementById('goals-completed');
+
 let savings = JSON.parse(localStorage.getItem("savings"))||  [];
-console.log(savings)
+let completedGoals =JSON.parse(localStorage.getItem('completedGoals')) || [];
+let emojis = JSON.parse(localStorage.getItem('emojis')) || [];
+let target = [];
+
+console.log(savings, emojis, target)
 
 
+ 
 
 
 
@@ -17,7 +30,17 @@ goalBtn.addEventListener('click', function(){
 
     if(goalInput.value.trim() === "") return;
 
-   savings.push(goalInput.value.trim())
+    const goal  = {
+        name : goalInput.value.trim(),
+        cost : Number(goalCost.value),
+        deadline: goalDeadline.value,
+        emoji : goalEmoji.value || "🎯",
+        completed : false
+    }
+
+   savings.push(goal)
+
+   
 
   
    
@@ -31,20 +54,96 @@ goalBtn.addEventListener('click', function(){
    renderItems()
    
     goalInput.value = '';
-
+    goalCost.value = '';
+    goalDeadline.value = '';
+    goalEmoji.value = '';
+   
 
 })
 
 function renderItems(){
+
     goalList.innerHTML = '';
-    let li = '';
+     
 
-     for( i = 0; i < savings.length ; i++){
+    if(savings.length === 0){
+        goalList.innerHTML = `there are NO items in the goal list`
+        return;
+    }
 
-       li += `<li>${savings[i]}</li>`
+    savings.forEach((goal,index) => {
+       let li = document.createElement("li")
+       li.className = 'goal-card';
 
-     }
-   goalList.innerHTML += li
-}
+          li.innerHTML= `
+                <h4>${goal.emoji}  ${goal.name}</h4>
+                <p> target: $${goal.cost}</p>
+                <p> 📅 Deadline: ${goal.deadline}</p>
+
+                <div class= "goal-actions"> 
+
+                    <button class="complete-btn" >Mark Complete</button>
+                    <button class="remove-btn">Remove</button>
+                
+                
+                </div>
+
+
+                
+                 
+            </li>`
+
+
+            li.querySelector('.complete-btn').onclick = () =>{
+
+            saving.splice(index, 1)[0]
+            completedGoal.complete = true
+            completedGoals.push(completedGoal)
+
+            localStorage.setItem('savings', JSON.stringify(savings))
+            localStorage.setItem('completedGoals', JSON.stringify(completedGoals))
+
+            alert("🎉 Goal completed!")
+
+            renderItems()
+
+
+        }
+
+        li.querySelector('.remove-btn').onclick =()=>{
+
+
+            savings.splice(index, 1)
+
+            renderItems()
+
+        }
+
+        goalList.appendChild(li);
+
+
+        
+    });
+
+    
+
+
+    }
+
+
+
+
+
+
+    
+    
+
+
+
+
+
+
+
+
 
 
